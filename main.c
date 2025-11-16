4a50b2349e7bd0374d6f7778394f8090b403ddd0
C027 <sadikaricimail@gmail.com>
2025-11-16 05:06:47 +0300

    Refactor main.c and add board solving functions
    Modularized main.c by splitting logic into multiple functions for argument checking, memory allocation, board printing, and solving. Added stubs and partial implementations for board solving helpers (do_top, do_bottom, do_left, do_right, fill_guaranteed_indexs, fill_other_indexs). Improved memory management and error handling.

--- main.c
+++ main.c
@@ -2,73 +2,33 @@
 #include <stdlib.h>
 
 void print_board(int **game);
+void fill_guaranteed_indexs(int **game, int **board);
+void check_argc(int arg_count, char **arg_values);
+void check_argv(int arg_count, char **arg_values, int i, int last, int x, int **game);
+void malloc_game(int ***game);
+void malloc_board(int ***board);
+void fill_other_indexs(int **game, int **board);
+void solve(int **board, int **game);
+void finish_program(int **game, int **board);
+void do_top(int **game, int **board, int col);
+void do_bottom(int **game, int **board, int col);
+void do_left(int **game, int **board, int row);
+void do_right(int **game, int **board, int row);
 
 int	main(int arg_count, char **arg_values)
 {
 	int	**game;
-	int	i;
-	int	x;
-	int	row;
-	int	col;
+    int **board;
 
-    i = 0;
-    x = 0;
-    row = 0;
-    col = 0;
-	if (arg_count != 2)
-	{
-		write(1, "error\n", 6);
-		return (1);
-	}
-	game = malloc(4 * sizeof(int *));
-    while (i < 4)
-	{
-		game[i] = malloc(4 * sizeof(int));
-		if (!game[i])
-			return (1);
-		i++;
-	}
-	i = 0;
-	char prev = 0;
-    while (arg_values[1][x] && i < 16)
-    {
-		row = i / 4;
-	    col = i % 4;
-	    if (arg_values[1][x] >= '1' && arg_values[1][x] <= '4')
-	    {
-	    	game[row][col] = arg_values[1][x] - '0';
-    		i++;
-	    }
-	    else if (arg_values[1][x] == ' ' && prev == ' ')
-	    {
-	    	game[row][col] = -1;
-	    	i++;
-    	}
-	    else if (arg_values[1][x] != ' ')
-	    {
-	    	write(1, "gecersiz karakter\n", 18);
-	    	return (1);
-	    }
-	    prev = arg_values[1][x];
-    	x++;
-    }
-	if (i != 16)
-	{
-		write(1, "eksik veri\n", 11);
-		return (1);
-	}
-    print_board(game);
-    i = 0;
-    while (i < 4)
-    {
-	    free(game[i]);
-	    i++;
-    }
-    free(game);
-	    return (0);
+    check_argc(arg_count, arg_values);
+	malloc_game(&game);
+    malloc_board(&board);
+	check_argv(arg_count, arg_values, game);
+    solve(board, game);
+    finish_program(int **game, int **board);
 }
 
-void    print_board(int **game)
+void    print_board(int **board)
 {
     int i;
     int j;
@@ -80,11 +40,11 @@ void    print_board(int **game)
         j = 0;
         while (j < 4)
         {
-            if (game[i][j] == -1)
+            if (board[i][j] == -1)
                 write(1, ". ", 2);
             else
             {
-                c = game[i][j] + '0';
+                c = board[i][j] + '0';
                 write(1, &c, 1);
                 write(1, " ", 1);
             }
@@ -93,4 +53,178 @@ void    print_board(int **game)
         write(1, "\n", 1);
         i++;
     }
+}
+void solve(int **board, int **game)
+{
+    fill_guaranteed_indexs(game, board);
+    fill_other_indexs(game, board);
+    print_board(board);
+}
+void	fill_guaranteed_indexs(int **game, int **board)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 4)
+	{
+		// Top game
+		void do_top(int **game, int **board, int col);
+		// Bottom game
+		void do_bottom(int **game, int **board, int col);
+		// Left game
+		void do_left(int **game, int **board, int row);
+		// Right game
+		void do_right(int **game, int **board, int row);
+		i++;
+	}
+}
+void check_argc(int arg_count, char **arg_values)
+{
+    if (arg_count != 2)
+    {
+        write(1, "eksik veri\n", 6);
+        exit(1);
+    }
+    return (0);
+}
+void check_argv(int arg_count, char **arg_values, int **game)
+{
+    int row;
+    int col;
+    int x;
+    int i;
+    char last;
+
+    x = 0;
+    i = 0;
+    last = '\0';
+    while (arg_values[1][x] && i < 16)
+    {
+		row = i / 4;
+	    col = i % 4;
+	    if (arg_values[1][x] >= '1' && arg_values[1][x] <= '4')
+	    {
+	    	game[row][col] = arg_values[1][x] - '0';
+    		i++;
+	    }
+	    else if (arg_values[1][x] == ' ' && last == ' ')
+	    {
+	    	game[row][col] = -1;
+	    	i++;
+    	}
+	    else if (arg_values[1][x] != ' ')
+	    {
+	    	write(1, "gecersiz karakter\n", 18);
+	    	return (1);
+	    }
+	    last = arg_values[1][x];
+    	x++;
+    }
+	if (i != 16)
+	{
+		write(1, "eksik veri\n", 11);
+		return (1);
+	}
+}
+void finish_program(int **game, int **board)
+{
+    int i;
+    i = 0;
+    while (i < 4)
+    {
+        free(board[i]);
+        i++;
+    }
+    i = 0;
+    while (i < 4)
+    {
+        free(board[i]);
+        i++;
+    }
+    free(board);
+    free(game);
+}
+void    malloc_game(int ***game)
+{
+    int i;
+
+    *game = (int **)malloc(sizeof(int *) * 4);
+    i = 0;
+    while (i < 4)
+    {
+        (*game)[i] = (int *)malloc(sizeof(int) * 4);
+        i++;
+    }
+}
+void    malloc_board(int ***board)
+{
+    int i;
+
+    *board = (int **)malloc(sizeof(int *) * 4);
+    i = 0;
+    while (i < 4)
+    {
+        (*board)[i] = (int *)malloc(sizeof(int) * 4);
+        i++;
+    }
+}
+void do_top(int **game, int **board, int col)
+{
+    if (game[0][i] == 1)
+			board[0][i] = 4;
+	else if (game[0][i] == 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			board[j][i] = j + 1;
+			j++;
+		}
+	}
+}
+void do_bottom(int **game, int **board, int col)
+{
+    if (game[1][i] == 1)
+			board[3][i] = 4;
+	else if (game[1][i] == 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			board[3 - j][i] = j + 1;
+			j++;
+		}
+	}
+}
+void do_left(int **game, int **board, int row)
+{
+    if (game[2][i] == 1)
+            board[i][0] = 4;
+    else if (game[2][i] == 4)
+    {
+        j = 0;
+        while (j < 4)
+        {
+            board[i][j] = j + 1;
+            j++;
+        }
+    }
+}
+void do_right(int **game, int **board, int row)
+{
+    if (game[3][i] == 1)
+            board[i][3] = 4;
+    else if (game[3][i] == 4)
+    {
+        j = 0;
+        while (j < 4)
+        {
+            board[i][3 - j] = j + 1;
+            j++;
+        }
+    }
+}
+void fill_other_indexs(int **game, int **board)
+{
 }
